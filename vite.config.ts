2d4956ca9e4aa19b25d7443aaf709645a41e8db2
Ishwarya Saravanan <126944909+Ishwarya-4@users.noreply.github.com>
2026-06-29 17:51:17 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/Report_Dashboard/',
+  base: mode === 'production' ? '/Report_Dashboard/' : '/',
   server: {
     host: "::",
     port: 8080,
